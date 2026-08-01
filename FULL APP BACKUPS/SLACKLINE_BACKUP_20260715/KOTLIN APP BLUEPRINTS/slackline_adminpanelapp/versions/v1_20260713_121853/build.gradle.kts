0bb78b724c6a1867ad63da8b150c5639dc444638
cryptowolfman97 <cryptowolfman97@gmail.com>
2026-08-01 14:28:27 +0530

    Delete FULL APP BACKUPS/SLACKLINE_BACKUP_20260715 directory

--- FULL APP BACKUPS/SLACKLINE_BACKUP_20260715/KOTLIN APP BLUEPRINTS/slackline_adminpanelapp/versions/v1_20260713_121853/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-}
-tasks.register<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
